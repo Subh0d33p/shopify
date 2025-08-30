feef928cc14caa74dc150fd95e9be9aa779ad6ef
Subhodeep Roy <subhodeeproy260@gmail.com>
2025-08-30 17:50:18 +0530

    $th commit?

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-import { createRequestHandler } from "@remix-run/netlify";
+// import { createRequestHandler } from "@remix-run/netlify";
 
-export const handler = createRequestHandler({
+// export const handler = createRequestHandler({
+//   build: require("./build"),
+//   mode: process.env.NODE_ENV,
+// });
+
+
+import { createRequestHandler } from "@netlify/remix-adapter";
+
+export default createRequestHandler({
   build: require("./build"),
   mode: process.env.NODE_ENV,
 });
