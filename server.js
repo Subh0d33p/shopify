4a5bee4bce2c0c8681ba6135a90c02db284d6008
Subhodeep Roy <subhodeeproy260@gmail.com>
2025-08-30 18:04:59 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-// import { createRequestHandler } from "@remix-run/netlify";
-
-// export const handler = createRequestHandler({
-//   build: require("./build"),
-//   mode: process.env.NODE_ENV,
-// });
-
-
 import { createRequestHandler } from "@netlify/remix-adapter";
 
 export default createRequestHandler({
